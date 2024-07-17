b42e65f37f2d81861e056ea198805ab2cf45b9c9
Pedro Avelar <pedroemanuel5296@gmail.com>
2024-07-17 10:58:06 -0300

    template de conexão ao db

--- app/backend/database/connection/connect.inc.php
+++ app/backend/database/connection/connect.inc.php
@@ -1,1 +1,15 @@
-<?php
+<?php 
+
+// Informações de acesso ao banco de dados
+$host = "localhost";
+$dbname = "EventControlDB";
+$username = "your_username_db";
+$password = "your_password_db";
+
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Verifica se a conn foi feita
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
