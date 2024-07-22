fd9905f715a19310775d790e132f92e7033192fd
Pedro Avelar <pedroemanuel5296@gmail.com>
2024-07-22 19:32:31 -0300

    força o role_id vim como 1 ou seja estudante

--- app/backend/class/Usuario/usuario.class.php
+++ app/backend/class/Usuario/usuario.class.php
@@ -21,13 +21,19 @@ class Usuario {
 
     public function create_user($data) {
         $user_pass = $data['senha'];
+        $role_id = 1;
+
+        if(empty($role_id)) {
+            $role_id = 1;
+        }
 
         // Criptografando a senha
         $user_pass_hash = password_hash($user_pass, PASSWORD_DEFAULT);
         // bind_params prepare evita sql_injection no backend https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+
         $query = "INSERT INTO " . $this->table_name . " (email, senha, nome, role_id) VALUES (?, ?, ?, ?)";
         $stmt = $this->conn->prepare($query);
-        $stmt->bind_param("sssi", $data['email'], $user_pass_hash, $data['nome'], $data['role_id']);
+        $stmt->bind_param("sssi", $data['email'], $user_pass_hash, $data['nome'], $role_id);
         if ($stmt->execute()) {
             return true;
         } else {
