7c63b492119d6d95fde2ade5fdff0c114c59b499
Pedro Avelar <pedroemanuel5296@gmail.com>
2024-08-26 10:11:43 -0300

    Adicionado metodo de update (edita) e delete (Apagar)

--- app/backend/class/CursosEvento/cursoseventos.class.php
+++ app/backend/class/CursosEvento/cursoseventos.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Classe CursosEventos
- * Criação
+ * Criação, Update, Delete
  */ 
 class CursosEvento {
     private $conn;
@@ -13,17 +13,38 @@ class CursosEvento {
     }
 
     public function create_curso_evento($data) {
-        // Verifica se o estudante já está inscrito em outro curso no mesmo horário
-        if ($this->is_horario_conflitante($data)) {
-            return false;
-        }
-
         $query = "INSERT INTO " . $this->table_name . " (id_evento, id_curso, quantidade_vagas, quantidade_inscritos, id_administrador, data, horario_inicio, horario_fim, data_criacao) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         $data_criacao = date('Y-m-d');
 
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("iiiiissss", $data['id_evento'], $data['id_curso'], $data['quantidade_vagas'], $data['quantidade_inscritos'], $data['id_administrador'], $data['data'], $data['horario_inicio'], $data['horario_fim'], $data_criacao);
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function update_curso_evento($data) {
+        $query = "UPDATE " . $this->table_name . " SET quantidade_vagas = ?, quantidade_inscritos = ?, data = ?, horario_inicio = ?, horario_fim = ? WHERE id_evento = ? AND id_curso = ?";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("iisssii", $data['quantidade_vagas'], $data['quantidade_inscritos'], $data['data'], $data['horario_inicio'], $data['horario_fim'], $data['id_evento'], $data['id_curso']);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete_curso_evento($id_evento, $id_curso) {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id_evento = ? AND id_curso = ?";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("ii", $id_evento, $id_curso);
+
         if ($stmt->execute()) {
             return true;
         } else {
